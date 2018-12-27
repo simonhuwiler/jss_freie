8c57471589b51473a1e4240dbcf224ca3e7d6b4e
nanoteilchen <webmaster@simonhuwiler.ch>
2018-12-27 15:32:50 +0100

    searchbar implemented

--- client/src/index.js
+++ client/src/index.js
@@ -15,79 +15,100 @@ function Line(props)
       </div>
     )
   }
-
-  /*
-  return (
-    <div className="inner_float">
-      <div className="icondiv"><img src={require("./img/" + props.icon)} className="icon" alt={props.text}/></div>
-      <span className="icontext" style={props.style}> {props.text}</span>
-    </div>
-  )
-    */
 }
 
-class Entry extends React.Component
+function Entry(props)
+{
+  let data = props.data;
+  var format = require('format-number');
+  let lohn = data.lohn ? format({ suffix: '.-', integerSeparator : "'"})(data.lohn, {noSeparator: false}) : '';
+  let display = props.visible == true ?  {} : {display: 'none'};
+
+  return (
+    <div className="container" style={display}>
+      <div className="cell container_image">
+        <img src={require("./logos/20min.png")} alt="logos/tmp.png" className="image_medium" />
+      </div>
+      <div className="cell container_right">
+        <p className="medium">{data.medium} - {data.ressort}</p>
+
+        <Line title='Jahr' text={data.jahr}/>
+        <Line title='Lohn' text={lohn}/>
+        <Line title='Sozialleistungen' text={data.sozialleistungen} />
+        <Line title='Spesen' text={data.spesen} />
+        <Line title='Abrechnung' text={data.abrechnung} />
+        <Line title='Auftrag' text={data.auftrag} />
+        <Line title='Zeichen' text={data.zeichen} />
+        <Line title='Infos' text={data.infos} />
+
+      </div>
+    </div>
+    
+  )
+}
+
+class List extends React.Component
 {
   constructor(props)
   {
     super(props);
+    //this.state = props;
   }
 
   render()
   {
-    let data = this.props.data;
-    var format = require('format-number');
-    let lohn = data.lohn ? format({ suffix: '.-', integerSeparator : "'"})(data.lohn, {noSeparator: false}) : '';
+    let entries = this.props.data.data.map((item, step) => {
+      let visible = true;
+      if(this.props.search && this.props.search != "")
+      {
+        let s = this.props.search.toLowerCase();
+        visible = item.medium.toLowerCase().indexOf(s) >= 0 || item.ressort.toLowerCase().indexOf(s) >= 0 ? true : false;
+      }
+      return (
+        <Entry key={step} data={item} visible={visible}/>
+      )
+    });
 
     return (
-      <div className="container">
-        <div className="cell container_image">
-          <img src={require("./logos/20min.png")} alt="logos/tmp.png" className="image_medium" />
-        </div>
-        <div className="cell container_right">
-          <p className="medium">{data.medium} - {data.ressort}</p>
-
-          <Line title='Lohn' text={lohn}/>
-          <Line title='Sozialleistungen' text={data.sozialleistungen} />
-          <Line title='Spesen' text={data.spesen} />
-          <Line title='Abrechnung' text={data.abrechnung} />
-          <Line title='Auftrag' text={data.auftrag} />
-          <Line title='Zeichen' text={data.zeichen} />
-          <Line title='Infos' text={data.infos} />
-
+      <div>
+        <div id='listBox'>
+          {entries}
         </div>
       </div>
-      
     )
   }
 }
 
-
-function List(props)
+class Search extends React.Component
 {
-  let entries = props.data.data.map((item, step) => {
-    return (
-      <Entry key={step} data={item}/>
-    )
-  });
-/*
-  const moves = history.map((step, move) => {
-    const desc = move ?
-      'Go to move #' + move :
-      'Go to game start';
-    return (
-      <li>
-        <button onClick={() => this.jumpTo(move)}>{desc}</button>
-      </li>
-    );
-  });
-  */
+  constructor(props)
+  {
+    super(props);
+    this.onSearch = props.onSearch;
+    this.state = {
+      search: ""
+    }
+  }
 
-  return (
-    <div id='listBox'>
-      {entries}
-    </div>
-  )
+  onChange(event)
+  {
+    clearInterval(this.interval);
+    let interval = setInterval(() => {
+      clearInterval(interval);
+      this.onSearch(this.state.search);
+    }, 500);
+    this.interval = interval;
+    this.setState({search: event.target.value});
+  }
+
+  render()
+  {
+    return (
+      <div className="search">
+        <input id="typesearch" type="text" value={this.state.search} name="search" placeholder="Suchen" onChange={this.onChange.bind(this)}/>
+      </div>
+    )
+  }
 }
 
 class Form extends React.Component
@@ -105,7 +126,6 @@ class Form extends React.Component
 
   componentDidMount()
   {
-    console.log("monut");
     fetch("data/data_bulk.json")
     .then(res => res.json())
     .then(
@@ -125,12 +145,16 @@ class Form extends React.Component
           error
         });
       }
-    )
+    );
+  }
+
+  search(key)
+  {
+    this.setState({search: key})
   }
 
   render()
   {
-    console.log("render");
     const { error, isLoaded, data } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -140,8 +164,9 @@ class Form extends React.Component
 
       return (
         <div>
+          <Search onSearch={e => this.search(e)}/>
           <p id='leadin'></p>
-          <List data={data}/>
+          <List data={data} search={this.state.search}/>
         </div>
       )
 
